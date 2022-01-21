e4585d851bc2ceffeac502d771cda25202592841
dandanfre <80621209+dandanfre@users.noreply.github.com>
2022-01-21 12:22:50 +0100

    Add files via upload
    Added Navbar and animation styling to necessary interactions

---
+++ navbar.js
@@ -0,0 +1,12 @@
+//get header and footer in document of the HTML
+const header = document.getElementById('navbar');
+
+//set in the structure of the html-elements in header 
+header.innerHTML=`
+    <a href="startside.html" id="navbarLogo"><img src="/images/homebutton.png" alt="Homebutton" width="300" height="100"></a>
+    <div class='navbarLinks'>
+        <a href="Programoversikt.html" class="navbarLink"><span class="un">Program</span></a>
+        <a href="VåreSPonsorer.html" class="navbarLink"><span class="un">Våre Sponsorer</span></a>
+        <a href="startside.html#epost_container" class="navbarLink"><span class="un">Kontakt</span></a>
+    </div>
+`
